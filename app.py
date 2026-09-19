9d8366dcdc7d5f4a611037825c6b492e0dfdedcc
dadhichgrv <dadhich.grv@gmail.com>
2026-09-19 12:24:26 +0530

    testing

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ load_dotenv()
 tenant_id = os.getenv("tenant_id")
 
 import sys
+print("prinitng")
 print("DEBUG SUBSCRIPTION_ID:", repr(os.getenv("SUBSCRIPTION_ID")), file=sys.stderr, flush=True)
 print("DEBUG RESOURCE_GROUP:", repr(os.getenv("RESOURCE_GROUP")), file=sys.stderr, flush=True)
 print("DEBUG ML_WORKSPACE_NAME:", repr(os.getenv("ML_WORKSPACE_NAME")), file=sys.stderr, flush=True)
